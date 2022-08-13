9b228c436df0a6df1840a418e613a0d437c1001e
nkere30 <ninokereselidze30@gmail.com>
2022-08-13 05:43:05 +0400

    Completed

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,36 @@
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
+    public static void printHourglassOfGivenSize(int height) {
+        for(int i = 0; i < height/2; i++) {
+            for(int k = 0; k < i; k++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < height - 2 * i; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
 
+        if(height % 2 != 0) {
+            for(int i = 0; i < height/2; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("8");
+            System.out.println();
+        }
+        for(int i = height/2; i > 0; i--) {
+            for(int k = i - 1; k > 0; k--) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j <= height - 2 * i + 1; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printHourglassOfGivenSize(20);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -1,7 +1,20 @@
 package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
-    public void printRectangleFrom8s(int length, int height){
+    public static void printRectangleFrom8s(int length, int height){
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < length; j++) {
+                if(j == 0 || j + 1 == length || i == 0 || i + 1 == height) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        printRectangleFrom8s(5, 6);
     }
 }
